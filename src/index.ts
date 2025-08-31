0dc2d627b322fc24ab6333ab4b7bd65b747ea6da
Agustín Gil <agustingilsosa@gmail.com>
2025-08-31 17:57:21 -0400

    return err.message

--- src/index.ts
+++ src/index.ts
@@ -46,9 +46,11 @@ app.get("/dolar", async (c) => {
 
 app.onError((err, c) => {
   const statusCode: StatusCode = 500;
-  const content: Record<string, unknown> = { message: "Internal server error" };
+  //const content: Record<string, unknown> = { message: "Internal server error" };
+
   console.error(err);
-  return c.json(content, statusCode);
+
+  return c.json(err.message, statusCode);
 });
 
 app.notFound((c) => {
